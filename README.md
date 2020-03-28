8a7734c8fe77575bb2114338c0962624222412f4
kuri <bunnkichi328@gmail.com>
2020-03-28 23:31:56 +0900

    専用のリポジトリの作成　現在できているところまでうｐ

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# sqlserverworkspace
+# sql検証用
+
+## 概要
+とある事情で非正規化されているテーブルを使用する事になったので自習ように自分で構築し、検証してみる。
+
+## ルール
+* excelファイルに関しては、gitに上げない(せっかくのバージョン管理ツールなので)
+* master&developブランチに直接のコミットはしない(git運用フローをやってみる)
+
+## table
+query\create_table
+
+## data
+query\insert
+
+## select
+query\select
+
+## 正規形
+[ボイスコット正規形](https://qiita.com/gooddoog/items/f40a7f0602bbe6afa1cf)
+
+## memo
+日本語をinsertする際はNをつける。(文字化け防止)
---
+++ query/insert/dimension/insert_dimension_dealer.sql
@@ -0,0 +1,5 @@
+INSERT INTO dimension_dealer(dealer_code,dealer_name) VALUES ('10001',N'販売店_1');
+INSERT INTO dimension_dealer(dealer_code,dealer_name) VALUES ('10002',N'販売店_2');
+INSERT INTO dimension_dealer(dealer_code,dealer_name) VALUES ('10003',N'販売店_3');
+INSERT INTO dimension_dealer(dealer_code,dealer_name) VALUES ('10004',N'販売店_4');
+INSERT INTO dimension_dealer(dealer_code,dealer_name) VALUES ('10005',N'販売店_5');
